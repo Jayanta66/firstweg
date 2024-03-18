dcd1513d345d0674360e7013f64bc91c2e5a8542
Jayanta66 <jayhalder18@gmail.com>
2024-03-18 15:02:20 +0530

    This is web design project

--- src/main/resources/static/js/game/GusseMyNumber/jss.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'COrrect Number';
-
-document.querySelector('.number').textContent = 5;
-
-document.querySelector('.score').textContent = 5;
